a102a826b1506d3a9c22b12b23184a1d12abb16f
Lunamana00 <kth000928@naver.com>
2024-07-08 08:14:02 +0900

    finish calendar

--- server.dart
+++ server.dart
@@ -8,12 +8,17 @@ import 'package:path/path.dart' as p;
 void main() async {
   final router = Router();
 
+  // JSON 파일 경로 설정
   final jsonFilePath = p.join(Directory.current.path, './db/users.json');
+  // 유저 폴더 기본 경로 설정
   final userFolderBasePath = p.join(Directory.current.path, './db/users');
 
+  // 회원가입 post 시작
   router.post('/register', (Request request) async {
     final payload = await request.readAsString();
     final newUser = jsonDecode(payload);
+
+    print('Received newUser: $newUser');
 
     File file = File(jsonFilePath);
     List<dynamic> users = [];
@@ -35,15 +40,17 @@ void main() async {
     final existingUser = users.firstWhere((user) => user['id'] == newUser['id'], orElse: () => null);
 
     if (existingUser != null) {
-      return Response(101);
+      return Response(101); // id 중복 오류
     }
 
     users.add(newUser);
+    print('Number of users: ${users.length}');
     await file.writeAsString(jsonEncode(users), mode: FileMode.write);
 
     final userFolder = Directory(p.join(userFolderBasePath, newUser['id']));
     if (!(await userFolder.exists())) {
       await userFolder.create(recursive: true);
+      print('User folder created: ${userFolder.path}');
     }
 
     final userJsonFile = File(p.join(userFolder.path, '${newUser['id']}.json'));
@@ -56,10 +63,10 @@ void main() async {
       'name': newUser['name'],
       'id': newUser['id'],
       'char_type': newUser['char_type'],
-      'u_lv': 0,
+      'u_lv': 1,
       'u_exp': 0
     };
-    final subjectData = {'lv': 0, 'exp': 0, 'dates': []};
+    final subjectData = {'lv': 1, 'exp': 0, 'dates': []};
 
     await userJsonFile.writeAsString(jsonEncode(userInitialData), mode: FileMode.write);
     await AJsonFile.writeAsString(jsonEncode(subjectData), mode: FileMode.write);
@@ -70,6 +77,7 @@ void main() async {
     return Response(200);
   });
 
+  // 로그인 post 시작
   router.post('/login', (Request request) async {
     final payload = await request.readAsString();
     final loginData = jsonDecode(payload);
@@ -103,6 +111,7 @@ void main() async {
     }
   });
 
+  // 유저 정보 가져오기
   router.get('/user_info/<id>', (Request request, String id) async {
     final userFolder = Directory(p.join(userFolderBasePath, id));
     final userJsonFilePath = p.join(userFolder.path, '$id.json');
@@ -121,12 +130,14 @@ void main() async {
         final JsonData = await response_list[i].readAsString();
         response_data.add(jsonDecode(JsonData));
       }
+      print(response_data);
       return Response.ok(jsonEncode(response_data), headers: {'Content-Type': 'application/json'});
     } else {
       return Response(500, body: 'User data file not found');
     }
   });
 
+  // 기록 요청 저장
   router.post('/upload', (Request request) async {
     final payload = await request.readAsString();
     final data = jsonDecode(payload);
@@ -146,8 +157,10 @@ void main() async {
 
     if (!(await userFolder.exists())) {
       await userFolder.create(recursive: true);
+      print('Folder created: ${userFolder.path}');
     }
 
+    // JSON 파일 생성 및 기본 정보 저장
     final recordData = {
       'comment': comment,
       'images': images,
@@ -155,8 +168,10 @@ void main() async {
 
     await recordFile.writeAsString(jsonEncode(recordData), mode: FileMode.write);
 
+    // subject가 ETC가 아닌 경우에만 경험치 업데이트
     Map<String, dynamic> responseData = {};
     if (subject != 'ETC') {
+      // 사용자 폴더의 {유저이름}_{subject}.json 파일의 exp와 lv 업데이트
       final subjectExpPath = p.join(Directory.current.path, 'db', 'users', id, '${id}_$subject.json');
       final subjectExpFile = File(subjectExpPath);
       Map<String, dynamic> subjectExpData;
@@ -164,7 +179,7 @@ void main() async {
         subjectExpData = jsonDecode(await subjectExpFile.readAsString());
         subjectExpData['dates'] = subjectExpData['dates'] ?? [];
       } else {
-        subjectExpData = {'lv': 0, 'exp': 0, 'dates': []};
+        subjectExpData = {'lv': 1, 'exp': 0, 'dates': []};
       }
 
       if (!subjectExpData['dates'].contains(date)) {
@@ -182,6 +197,7 @@ void main() async {
       responseData['subjectExp'] = subjectExpData['exp'];
       responseData['subjectLevel'] = subjectExpData['lv'];
 
+      // 사용자 폴더의 {유저이름}.json 파일의 exp와 lv 업데이트
       final userExpPath = p.join(Directory.current.path, 'db', 'users', id, '$id.json');
       final userExpFile = File(userExpPath);
       if (await userExpFile.exists()) {
@@ -203,8 +219,10 @@ void main() async {
     return Response.ok(jsonEncode(responseData), headers: {'Content-Type': 'application/json'});
   });
 
+  // 특정 날짜의 기록 가져오기
   router.get('/record/<id>/<date>', (Request request, String id, String date) async {
     final recordFolder = Directory(p.join(Directory.current.path, 'db', 'users', id, date));
+    print(id);
     if (await recordFolder.exists()) {
       final records = recordFolder
           .listSync()
