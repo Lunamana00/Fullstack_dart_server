c2bbe0568f66966459aff43d3c6b8daec311a803
Lunamana00 <kth000928@naver.com>
2024-07-08 03:52:09 +0900

    upload json

--- server.dart
+++ server.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_router/shelf_router.dart';
@@ -16,14 +15,11 @@ void main() async {
 
   // 회원가입 post 시작
   router.post('/register', (Request request) async {
-    // 요청의 본문을 읽어서 JSON 데이터로 디코딩
     final payload = await request.readAsString();
     final newUser = jsonDecode(payload);
 
-    // newUser 출력
     print('Received newUser: $newUser');
 
-    // 기존 JSON 파일 읽기
     File file = File(jsonFilePath);
     List<dynamic> users = [];
 
@@ -37,37 +33,26 @@ void main() async {
           }
         }
       } catch (e) {
-        // JSON 파싱 오류 처리
         print('Error reading JSON file: $e');
       }
     }
 
-    // ID 중복 확인
-    final existingUser = users.firstWhere((user) => user['id'] == newUser['id'],
-        orElse: () => null);
+    final existingUser = users.firstWhere((user) => user['id'] == newUser['id'], orElse: () => null);
 
     if (existingUser != null) {
-      // ID가 중복된 경우
       return Response(101); // id 중복 오류
     }
 
-    // 새로운 사용자 추가
     users.add(newUser);
-
-    // 등록 유저 수 출력
     print('Number of users: ${users.length}');
-
-    // JSON 파일에 저장
     await file.writeAsString(jsonEncode(users), mode: FileMode.write);
 
-    // 유저 폴더 생성
     final userFolder = Directory(p.join(userFolderBasePath, newUser['id']));
     if (!(await userFolder.exists())) {
       await userFolder.create(recursive: true);
       print('User folder created: ${userFolder.path}');
     }
 
-    // 유저 JSON 파일 생성 및 기본 정보 저장
     final userJsonFile = File(p.join(userFolder.path, '${newUser['id']}.json'));
     final AJsonFile = File(p.join(userFolder.path, '${newUser['id']}_A.json'));
     final BJsonFile = File(p.join(userFolder.path, '${newUser['id']}_B.json'));
@@ -83,20 +68,14 @@ void main() async {
     };
     final subjectData = {'lv': 1, 'exp': 0};
 
-    await userJsonFile.writeAsString(jsonEncode(userInitialData),
-        mode: FileMode.write);
-    await AJsonFile.writeAsString(jsonEncode(subjectData),
-        mode: FileMode.write);
-    await BJsonFile.writeAsString(jsonEncode(subjectData),
-        mode: FileMode.write);
-    await CJsonFile.writeAsString(jsonEncode(subjectData),
-        mode: FileMode.write);
-    await DJsonFile.writeAsString(jsonEncode(subjectData),
-        mode: FileMode.write);
+    await userJsonFile.writeAsString(jsonEncode(userInitialData), mode: FileMode.write);
+    await AJsonFile.writeAsString(jsonEncode(subjectData), mode: FileMode.write);
+    await BJsonFile.writeAsString(jsonEncode(subjectData), mode: FileMode.write);
+    await CJsonFile.writeAsString(jsonEncode(subjectData), mode: FileMode.write);
+    await DJsonFile.writeAsString(jsonEncode(subjectData), mode: FileMode.write);
 
     return Response(200);
   });
-  // 회원가입 post 완료
 
   // 로그인 post 시작
   router.post('/login', (Request request) async {
@@ -126,13 +105,11 @@ void main() async {
     );
 
     if (existingUser != null) {
-      return Response.ok(jsonEncode(existingUser),
-          headers: {'Content-Type': 'application/json'});
+      return Response.ok(jsonEncode(existingUser), headers: {'Content-Type': 'application/json'});
     } else {
       return Response(201);
     }
   });
-  //로그인 post 종료
 
   // 유저 정보 가져오기
   router.get('/user_info/<id>', (Request request, String id) async {
@@ -145,13 +122,7 @@ void main() async {
     final CJsonFile = File(p.join(userFolder.path, '${id}_C.json'));
     final DJsonFile = File(p.join(userFolder.path, '${id}_D.json'));
 
-    var response_list = [
-      userJsonFile,
-      AJsonFile,
-      BJsonFile,
-      CJsonFile,
-      DJsonFile
-    ];
+    var response_list = [userJsonFile, AJsonFile, BJsonFile, CJsonFile, DJsonFile];
 
     if (await userJsonFile.exists()) {
       var response_data = [];
@@ -160,15 +131,46 @@ void main() async {
         response_data.add(jsonDecode(JsonData));
       }
       print(response_data);
-      return Response.ok(jsonEncode(response_data),
-          headers: {'Content-Type': 'application/json'});
+      return Response.ok(jsonEncode(response_data), headers: {'Content-Type': 'application/json'});
     } else {
       return Response(500, body: 'User data file not found');
     }
   });
 
-  final handler =
-      const Pipeline().addMiddleware(logRequests()).addHandler(router);
+  // 기록 요청 저장
+  router.post('/upload', (Request request) async {
+    final payload = await request.readAsString();
+    final data = jsonDecode(payload);
+
+    final String id = data['id'] ?? 'unknown';
+    final DateTime parsedDate = DateTime.parse(data['date']);
+    final String date = "${parsedDate.year}${parsedDate.month.toString().padLeft(2, '0')}${parsedDate.day.toString().padLeft(2, '0')}"; // 날짜를 yyyyMMdd 형식으로 변환
+    final String icon = data['icon'] ?? 'unknown';
+    final List<String> images = data['images'] != null ? List<String>.from(data['images']) : [];
+    final String comment = data['comment'] ?? '';
+
+    final SavePath = p.join(Directory.current.path, 'db', 'users', id, date, '$icon.json');
+
+    final userFolder = Directory(p.dirname(SavePath));
+    if (!(await userFolder.exists())) {
+      await userFolder.create(recursive: true);
+      print('Folder created: ${userFolder.path}');
+    }
+
+    // JSON 파일 생성 및 기본 정보 저장
+    final recordFile = File(SavePath);
+
+    final recordData = {
+      'comment': comment,
+      'images': images,
+    };
+
+    await recordFile.writeAsString(jsonEncode(recordData), mode: FileMode.write);
+
+    return Response(200);
+  });
+
+  final handler = const Pipeline().addMiddleware(logRequests()).addHandler(router);
 
   final server = await io.serve(handler, '0.0.0.0', 8080);
 
